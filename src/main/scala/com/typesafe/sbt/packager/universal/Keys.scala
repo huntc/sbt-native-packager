7d9918d25b1860910cd398e00d3a7065b0b165c3
Christopher Hunt <huntchr@gmail.com>
2014-06-27 21:55:00 +1000

    Reactive Runtime bundling

--- src/main/scala/com/typesafe/sbt/packager/universal/Keys.scala
+++ src/main/scala/com/typesafe/sbt/packager/universal/Keys.scala
@@ -9,7 +9,7 @@ trait UniversalKeys {
   val packageZipTarball = TaskKey[File]("package-zip-tarball", "Creates a tgz package.")
   val packageXzTarball = TaskKey[File]("package-xz-tarball", "Creates a txz package.")
   val packageOsxDmg = TaskKey[File]("package-osx-dmg", "Creates a dmg package for OSX (only on osx).")
-  val stage = TaskKey[Unit]("stage", "Create a local directory with all the files laid out as they would be in the final distribution.")
+  val stage = TaskKey[File]("stage", "Create a local directory with all the files laid out as they would be in the final distribution.")
   val dist = TaskKey[File]("dist", "Creates the distribution packages.")
   val stagingDirectory = SettingKey[File]("stagingDirectory", "Directory where we stage distributions/releases.")
 }
@@ -19,9 +19,11 @@ object Keys extends UniversalKeys {
   def packageBin = sbt.Keys.packageBin
   def packageSrc = sbt.Keys.packageSrc
   def packageDoc = sbt.Keys.packageDoc
+  def moduleID = sbt.Keys.moduleID
   def name = sbt.Keys.name
   def packageName = packager.Keys.packageName
   def normalizedName = sbt.Keys.normalizedName
+  def projectInfo = sbt.Keys.projectInfo
   def target = sbt.Keys.target
   def sourceDirectory = sbt.Keys.sourceDirectory
   def streams = sbt.Keys.streams
