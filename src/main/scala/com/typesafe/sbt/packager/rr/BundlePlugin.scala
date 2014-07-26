1be81805101d550d3c614a0fd914585f79baa1ea
Christopher Hunt <huntchr@gmail.com>
2014-07-26 16:01:23 +1000

    Corrected the calculation of the digest.

--- src/main/scala/com/typesafe/sbt/packager/rr/BundlePlugin.scala
+++ src/main/scala/com/typesafe/sbt/packager/rr/BundlePlugin.scala
@@ -1,6 +1,8 @@
 package com.typesafe.sbt.packager
 package rr
 
+import java.io.{ BufferedInputStream, FileInputStream }
+
 import sbt._
 
 import universal._
@@ -8,6 +10,8 @@ import universal.Keys._
 import rr.Keys._
 import java.security.MessageDigest
 import java.nio.charset.Charset
+
+import scala.annotation.tailrec
 
 trait BundlePlugin extends Plugin with UniversalPlugin {
   val ReactiveRuntime = config("rr") extend Universal
@@ -36,7 +40,6 @@ trait BundlePlugin extends Plugin with UniversalPlugin {
   val Utf8 = "UTF-8"
 
   val utf8Charset = Charset.forName(Utf8)
-  val digest = MessageDigest.getInstance(Sha256)
 
   private def getConfig: Def.Initialize[Task[String]] = Def.task {
 
@@ -50,7 +53,7 @@ trait BundlePlugin extends Plugin with UniversalPlugin {
     s"""
          |version = "1.0.0"
          |components = {
-         |  "${(name in Universal).value}" = {
+         |  "${(packageFilename in Universal).value}" = {
          |    description      = "${projectInfo.value.description}"
          |    cluster-role     = "${clusterRole.value}"
          |    file-system-type = "${bundleType.value}"
@@ -64,25 +67,43 @@ trait BundlePlugin extends Plugin with UniversalPlugin {
   private def createDist(bundleTypeConfig: Configuration): Def.Initialize[Task[File]] = Def.task {
     val bundleTarget = (target in ReactiveRuntime).value
     val configTarget = bundleTarget / "tmp"
-    def relParent(p: (File, String)): (File, String) = (p._1, (name in Universal).value + java.io.File.separator + p._2)
+    def relParent(p: (File, String)): (File, String) = (p._1, (packageFilename in Universal).value + java.io.File.separator + p._2)
     val configFile = writeConfig(configTarget, bundleConf.value)
     val bundleMappings =
       configFile.pair(relativeTo(configTarget)) ++
         (mappings in bundleTypeConfig).value.map(relParent)
-    val tgz = Archives.makeTgz(bundleTarget, (name in Universal).value, bundleMappings)
+    val tgz = Archives.makeTgz(bundleTarget, (packageFilename in Universal).value, bundleMappings)
     val tgzName = tgz.getName
     val exti = tgzName.lastIndexOf('.')
-    val hash = Hash.toHex(digest.digest(IO.read(tgz, utf8Charset).getBytes(utf8Charset)))
+    val hash = Hash.toHex(digestFile(tgz))
     val hashName = tgzName.take(exti) + "-" + hash + tgzName.drop(exti)
     val hashTgz = tgz.getParentFile / hashName
     IO.move(tgz, hashTgz)
     hashTgz
   }
 
+  private def digestFile(f: File): Array[Byte] = {
+    val digest = MessageDigest.getInstance(Sha256)
+    val in = new BufferedInputStream(new FileInputStream(f))
+    val buf = Array.ofDim[Byte](8192)
+    try {
+      @tailrec
+      def readAndUpdate(r: Int): Unit =
+        if (r != -1) {
+          digest.update(buf, 0, r)
+          readAndUpdate(in.read(buf))
+        }
+      readAndUpdate(in.read(buf))
+      digest.digest
+    } finally {
+      in.close()
+    }
+  }
+
   private def stageBundle(bundleTypeConfig: Configuration): Def.Initialize[Task[File]] = Def.task {
     val bundleTarget = (stagingDirectory in ReactiveRuntime).value
     writeConfig(bundleTarget, bundleConf.value)
-    val componentTarget = bundleTarget / (name in Universal).value
+    val componentTarget = bundleTarget / (packageFilename in Universal).value
     IO.copy((mappings in bundleTypeConfig).value.map(p => (p._1, componentTarget / p._2)))
     componentTarget
   }
